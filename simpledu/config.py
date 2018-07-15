f87dbecd17190770d480249ac1aa2869ee632051
Li Li <fywest2109@hotmail.com>
2018-07-15 11:31:05 +0800

    29_experiment

---
+++ simpledu/config.py
@@ -0,0 +1,24 @@
+class BaseConfig(object):
+    SECRET_KEY = 'makesure to set a very secret key'
+    INDEX_PER_PAGE=9
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/simpledu?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://fywest:990113@localhost:3306/simpledu?charset=utf8'
+
+
+class ProductionConfig(BaseConfig):
+    pass
+
+
+class TestingConfig(BaseConfig):
+    pass
+
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
+
